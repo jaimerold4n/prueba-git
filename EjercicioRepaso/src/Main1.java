b7c3a990854d74831ea739df9e81b8307d9f58ba
jaime roldan <jaime.roldan@opendeusto.es>
2024-09-20 08:37:45 +0200

    FEFEFEF

--- EjercicioRepaso/src/Main1.java
+++ EjercicioRepaso/src/Main1.java
@@ -1,5 +1,7 @@
 	public class Main1 {
 		public static void main1(String[] args){
-		System.out.println("AY JOHNY 123VIVIA LA ESPERANSA DE VIDA4");
+		System.out.println("AY JOHNY 123VIVIA LA ESPERANSA DE VIDA4"
+				+ ""
+				+ "FEFEFEFEFEFEF");
 	}}
 	
